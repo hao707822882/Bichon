78e7355a5feb6b97c2ee567458353f5e6a29188c
朱明明 <2428922347@qq.com>
2016-01-04 18:45:22 +0800

    安装模块

---
+++ com/timeTask/__init__.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+# --coding:utf-8--
+# coding: utf-8
+# ━━━━━━神兽出没━━━━━━
+#  　　　┏┓　　　┏┓
+#  　　┏┛┻━━━┛┻┓
+#  　　┃　　　　　　　┃
+#  　　┃　　　━　　　┃
+#  　　┃　┳┛　┗┳　┃
+#  　　┃　　　　　　　┃
+#  　　┃　　　┻　　　┃
+#  　　┃　　　　　　　┃
+#  　　┗━┓　　　┏━┛
+#  　　　　┃　　　┃神兽保佑, 永无BUG!
+#  　　　　┃　　　┃Code is far away from bug with the animal protecting
+#  　　　　┃　　　┗━━━┓
+#  　　　　┃　　　　　　　┣┓
+#  　　　　┃　　　　　　　┏┛
+#  　　　　┗┓┓┏━┳┓┏┛
+#  　　　　　┃┫┫　┃┫┫
+#  　　　　　┗┻┛　┗┻┛
+#  ━━━━━━感觉萌萌哒━━━━━━
+#  Module Desc:clover
+#  User: z.mm | 2428922347@qq.com
+#  Date: 2016/1/4
+#  Time: 18:37
+
+
+__author__ = 'Administrator'
+'''
+    定时任务
+        数据备份
+'''
